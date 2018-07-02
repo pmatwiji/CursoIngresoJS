6ec55ccee8a19174802583c5779c2ffe950aff6c
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-07-02 12:05:39 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,17 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	/*
+	esto
+	es
+	un
+	comentario
+	en bloque */
+
+	var nombre;
+	nombre = prompt("ingrese su nombre", "natalia natalia");
+	alert("Su nombre es: " + nombre);
+	//var person = prompt("Please enter your name", "Harry Potter");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -1,8 +1,12 @@
 /*Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function mostrar()
 {
-	
+	/*var nombre = "asasead"
+	console.log(nombre)*/
+	var nombre;
+	nombre = document.getElementById('elNombre').value;
+	alert(nombre);
 }
 
 
