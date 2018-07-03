d1c6ed92aaf5ea88902598815507d89cf9616177
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-07-02 21:52:13 -0300

    practica parcial 1 2 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,8 @@
 function mostrar()
 {
-
+    var precio;
+    var descuento;
+    precio = prompt("ingrese el precio");
+    descuento = prompt("ingrese el descuento");
+    
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,9 @@
 
 function mostrar()
 {
-
+    var ancho;
+    var alto;
+    ancho = prompt("ingrese el ancho");
+    alto = prompt("ingrese el alto");
+    alert("El perimetro es: "+ ((ancho*2) + (alto*2)));
 }
