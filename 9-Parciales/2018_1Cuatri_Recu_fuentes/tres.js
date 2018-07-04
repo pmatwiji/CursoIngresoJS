054c75a67e71be2a5da4271fcdb8732337716f17
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-07-04 12:22:31 -0300

    ejercicio tres

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,21 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var descuentoEnDinero;
+	var precioConDescuento;
+	var iva;
+	var precioFinal;
+	precio = prompt("Ingrese el precio"); 	//1000
+	precio = parseInt(precio);
+	descuento = prompt("ingrese el porcentaje de descuento");   //10
+	descuento = parseInt(descuento);
+	descuentoEnDinero = (precio * descuento) / 100;
+	precioConDescuento = precio - descuentoEnDinero;
+	iva = precioConDescuento * 1.21;
+	elPrecioFinal = 
+	alert("Su descuento es de " + descuentoEnDinero + " .Su precio con descuento es de " + precioConDescuento
+		+ " .El precio con IVA es de " + iva);
+	document.getElementById('elPrecioFinal')
 
 }
