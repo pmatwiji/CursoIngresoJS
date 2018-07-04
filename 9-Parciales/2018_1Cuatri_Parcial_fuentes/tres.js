2a282c720a41f27f0a179846281c0bc829f6ae8a
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-07-03 21:56:56 -0300

    ejercicio 3 completo

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -4,5 +4,7 @@ function mostrar()
     var descuento;
     precio = prompt("ingrese el precio");
     descuento = prompt("ingrese el descuento");
-    
+    var resultado;
+    resultado = precio - (precio/100 * descuento);
+    document.getElementById('elPrecioFinal').value = "el precio final es " + resultado;
 }
