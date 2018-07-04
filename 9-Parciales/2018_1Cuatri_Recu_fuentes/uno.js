c23e2f0dde656da496e1d6729ab075c012772c83
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-07-04 11:59:14 -0300

    Ejercicios 1 y 2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
+	var lado;
+    var superficie;
+    var perimetro;
+    lado = prompt("ingrese el tamaño");
+    perimetro = lado * 3;
+    superficie = (lado * lado) / 2;
+    alert("el perimetro del triangulo equilatero es de " + perimetro +" y su superficie es de " + superficie);
+
 
 }
