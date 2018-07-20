d8e3ebc6f1db352dc6056af9189fb945626a93d7
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-07-20 00:57:07 -0300

    Ejercicio 4

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,16 @@
-function mostrar()
-{
+function mostrar() {
+  var numeroUno = prompt("Ingrese el primer numero");
+  var numeroDos = prompt("Ingrese el segundo numero");
+  var resultado;
 
+  if (numeroUno == numeroDos) {
+    alert(numeroUno + numeroDos);
+  } else if (parseInt(numeroUno) > parseInt(numeroDos)) {
+    resultado = numeroUno / numeroDos;
+  } else {
+    resultado = parseInt(numeroUno) + parseInt(numeroDos);
+    if (resultado < 50) {
+      alert("la suma es " + resultado + " y es menor a 50");
+    }
+  }
 }
