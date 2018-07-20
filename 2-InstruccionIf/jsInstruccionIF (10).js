1dfb1c1c661dbb0c949c847c53714e61c4e25372
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-07-20 00:49:17 -0300

    Ejercicio 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+		var numero;
+
+		numero = Math.floor((Math.random() * 10) + 1);
+		
+		if (numero > 8) {
+			alert("Su nota es: " + numero + ". Excelente!");
+		}	else if (numero < 5) {
+			alert("Su nota es: " + numero + ". Vamos, la proxima se puede");
+		} else {
+			alert("Su nota es: " + numero + ". Aprobo ");
+		}
 
 }//FIN DE LA FUNCIÓN
