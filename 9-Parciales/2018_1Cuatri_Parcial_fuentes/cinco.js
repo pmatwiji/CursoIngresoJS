2aad1202ab594a2df50fc19f3ca4b9ef54ff544e
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-07-24 23:13:44 -0300

    Ejercicio 5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+  var planeta;
+  planeta = prompt("Ingrese un planeta");
+
+  switch (planeta) 
+  {
+    case "mercurio" :
+    case "venus" :
+      alert("Acá hace mas calor");
+      break;
+    case "tierra":
+      alert("Acá vivimos");
+      break;
+    case "marte" :
+    case "júpiter" :
+    case "saturno" :
+    case "urano" :
+    case "neptuno" :
+    case "plutón" :
+      alert("Acá hace mas frio");
+      break;
+    default:
+      alert("Este no es un planeta del sistema solar");
+      break;
+  }
 
 }
