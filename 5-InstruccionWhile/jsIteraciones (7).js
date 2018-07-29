c9dca25a3cac6c6517bb998032f580f53388bb5b
Pablo Matwijiszyn <pmatwiji@gmail.com>
2018-07-29 20:20:59 -0300

    Ejercicio 7

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,9 +4,27 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	respuesta = respuesta.toLowerCase();
+	var numero;
+	var promedio;
+	
+
+	while (respuesta == "si") 
+	{
+		numero = prompt("Ingrese un numero");
+		numero = parseInt(numero);
+		acumulador = acumulador + numero;
+		contador ++;
+		respuesta = prompt("Desea continuar? Ingrese Si o No");
+		respuesta = respuesta.toLowerCase();
+	}
+
+	promedio = acumulador / contador;
+
+
 
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+document.getElementById('promedio').value=promedio;
 
 }//FIN DE LA FUNCIÓN
